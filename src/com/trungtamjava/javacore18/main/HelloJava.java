59394b63946f2bbbabe3bbc400edfd44bc989642
Administrator <Administrator@S3PZOWMD1MS4CH3>
2020-03-02 23:28:38 +0900

    commit to github

---
+++ src/com/trungtamjava/javacore18/main/HelloJava.java
@@ -0,0 +1,13 @@
+package com.trungtamjava.javacore18.main;
+
+///line-comment
+///ten class: Viet hoa chu cai dau tien,ko dau
+public class HelloJava {
+    //ctr + space ;: suggest
+	//ctr + shift + f : format can dong code
+	//ctr + s : save code 
+	public static void main(String[] args) {
+		System.out.println("Hello Java");
+		System.out.println("Hello");
+	}
+}
